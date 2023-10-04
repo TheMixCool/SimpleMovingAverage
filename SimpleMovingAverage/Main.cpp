1ee5f30fcda3af60853aee5d2cf5f4786b2e8dd6
Egor <mixcraftcool@icloud.com>
2023-10-04 03:20:56 +0300

    initial commit

---
+++ SimpleMovingAverage/Main.cpp
@@ -0,0 +1,9 @@
+#include "SimpleMovingAverage.h"
+#include <vector>
+#include <iostream>
+
+int main() {
+	std::vector<int> v {1, 2, 3, 4, 5, 6};
+
+	SimpleMovingAverage(v);
+}
---
+++ SimpleMovingAverage/SimpleMovingAverage.h
@@ -0,0 +1,10 @@
+﻿#include <vector>
+#include <iostream>
+#pragma once
+
+template<class T>
+void SimpleMovingAverage(std::vector<T>& v, int windowSize = 4) {
+	for (auto& el : v) {
+		std::cout << el << '\n';
+	}
+}
