82e4e10f2c0d014a99489f7d4217d13e2dd2b403
Egor <mixcraftcool@icloud.com>
2023-10-04 22:38:17 +0300

    SimpleMovingAverage function v0.1

--- SimpleMovingAverage/SimpleMovingAverage.h
+++ SimpleMovingAverage/SimpleMovingAverage.h
@@ -1,10 +1,32 @@
-﻿#include <vector>
-#include <iostream>
+﻿#include <deque>
 #pragma once
 
 template<class T>
-void SimpleMovingAverage(std::vector<T>& v, int windowSize = 4) {
-	for (auto& el : v) {
-		std::cout << el << '\n';
+std::deque<T> EnterFirstValues(std::deque<T>& v, int windowSize, T &currentValue) {
+
+	std::deque<T> result;
+
+	for (int i = 0; i != windowSize; ++i) {
+		currentValue += v[i];
+		result.push_back(currentValue / (i+1));
 	}
+
+	return result;
 }
+
+template<class T>
+std::deque<T> SimpleMovingAverage(std::deque<T>& data, int windowSize) {
+
+	T currentValue = 0;
+
+	std::deque<T> result = EnterFirstValues(data, windowSize, currentValue);
+
+	for (int i = windowSize; i != data.size(); ++i) {
+		currentValue -= data[i - windowSize];
+		currentValue += data[i];
+		result.push_back(currentValue / windowSize);
+	}
+
+	return result;
+}
+
